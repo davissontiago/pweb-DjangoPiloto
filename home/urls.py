250ded645d87eba3dbb4b386fe4e46a20b09e1c5
davissontiago <davissontiagolf@gmail.com>
2025-12-07 19:00:01 -0300

    acompanhandon atividades

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
     path('ajuda/', views.ajuda, name='ajuda'),
+    path('produto/', views.produto, name='produto'),
     path('item/<int:id>', views.exibir_item, name='exibir_item'),
     path('perfil/<str:usuario>', views.perfil , name='perfil'),
     path('diasemana/<int:dia>', views.dia_semana, name='dia_semana'),
+    path('home/', views.home, name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -14,6 +14,27 @@ def contato(request):
 def ajuda(request):
     return render(request, 'ajuda.html')
 
+def produto(request):
+    contexto = {
+        'lista': [
+            {'id': 1, 'nome': 'Notebook', 'preco': '2.500,00'},
+            {'id': 2, 'nome': 'Monitor', 'preco': '500,00'},
+            {'id': 3, 'nome': 'Teclado', 'preco': '80,00'},
+            {'id': 4, 'nome': 'Mouse', 'preco': '40,00'},
+            {'id': 5, 'nome': 'Impressora', 'preco': '600,00'},
+            {'id': 6, 'nome': 'Scanner', 'preco': '700,00'},
+            {'id': 7, 'nome': 'Câmera Web', 'preco': '150,00'},
+            {'id': 8, 'nome': 'Headset', 'preco': '120,00'},
+            {'id': 9, 'nome': 'Pendrive 32GB', 'preco': '30,00'},
+            {'id': 10, 'nome': 'HD Externo 1TB', 'preco': '350,00'},
+            {'id': 11, 'nome': 'Estabilizador', 'preco': '200,00'},
+            {'id': 12, 'nome': 'Switch 8 portas', 'preco': '180,00'},
+            {'id': 13, 'nome': 'Roteador Wi-Fi', 'preco': '220,00'},
+        ],
+    }
+    return render(request, 'produto/list.html',contexto)
+
+
 def exibir_item(request, id):
     return render(request, 'exibir_item.html', {'id':id})
 
@@ -36,4 +57,12 @@ def dia_semana(request, dia):
     if dia_nome:
         return render(request, 'dia_semana.html', {'dia': dia_nome})
     else:
-        return HttpResponse("Dia inválido! Digite um número de 1 a 7.")
+        return render(request, 'dia_semana.html', {'dia': "Dia inválido"})
+    
+def home(request):
+    context = {
+        'username': 'Carlos',
+        'items': ['Lápis','Caneta','Borracha']
+    }
+
+    return render(request,'home.html',context)
